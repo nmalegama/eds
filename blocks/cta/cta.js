20048ab1fcece9bca1a07bee5a159c2e5678d7e1
Navaneetha <navaneetha.malegama@capella.edu>
2025-07-08 17:40:52 +0530

    cta js change

--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const ctaBtn = document.createElement('Button');
+/*  const ctaBtn = document.createElement('Button');
   const itm = [...block.children][0];
   const itmProps = [...itm.children][0];
 
@@ -7,7 +7,7 @@ export default function decorate(block) {
   var linkIcon = [...itmProps.children][1];
   var ctaIcon = document.createElement('span');
   
-  myLink.setAttribute('target', '_blank');
+  myLink.setAttribute('target', '_blank'); 
   [...block.children].forEach((row) => {
     var btnProps = [...row.children];
     [...row.children].forEach((div) => {
@@ -23,7 +23,7 @@ export default function decorate(block) {
     // + btnProps[1].children[0].innerHTML;
     //ctaBtn.setAttribute('target', btnProps[0].children[2].innerHTML);
     //ctaBtn.setAttribute('href', btnProps[0].children[3].innerHTML);
-  }); 
+  }); */
   //ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   //block.textContent = '';
   //console.log(btnProps);
