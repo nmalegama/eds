ba1109447e56384f10f8b708ce994fedec00bffe
Navaneetha <navaneetha.malegama@capella.edu>
2025-07-08 17:53:21 +0530

    build errors fixed for css files

--- blocks/imagetext/imagetext.js
+++ blocks/imagetext/imagetext.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const imgRow = block.firstElementChild;
-  imgRow.className = 'd-flex img-text__wrapper';
+  imgRow.className = 'd-flex img-text-wrapper';
   [...imgRow.children].forEach((div) => {
     div.className = 'imgtxt-div col-12 col-md-6';
   });
